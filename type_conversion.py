efd6df628dee4f0e43e4fc0039e2a8c33670977e
unknown <gazinafisrafi.gnr@gmail.com>
2025-02-10 00:52:09 +0600

    type conversion

---
+++ type_conversion.py
@@ -0,0 +1,15 @@
+# birth_year = input("What is your birth year? ")
+
+# age = 2025 - int(birth_year)
+
+# print("you are", age, "years old.")
+
+# exercise
+
+first_number = input("enter the first number: ")
+second_number = input("enter the second number: ")
+
+# result = int(first_number) + int(second_number)
+result = float(first_number) + float(second_number)
+
+print("summation is:", result)
