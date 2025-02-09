cfc1eb67b0d31d5be2144eddde1d1c1fdc94faa5
unknown <gazinafisrafi.gnr@gmail.com>
2025-02-10 00:54:41 +0600

    type conversion practiced

--- type_conversion.py
+++ type_conversion.py
@@ -1,8 +1,8 @@
-# birth_year = input("What is your birth year? ")
+birth_year = input("What is your birth year? ")
 
-# age = 2025 - int(birth_year)
+age = 2025 - int(birth_year)
 
-# print("you are", age, "years old.")
+print("you are", age, "years old.")
 
 # exercise
 
@@ -13,3 +13,8 @@ second_number = input("enter the second number: ")
 result = float(first_number) + float(second_number)
 
 print("summation is:", result)
+
+is_online = True
+
+converted_text = str(is_online)
+print(is_online, converted_text)
