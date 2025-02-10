0a3d85cb9fc99ee0fd21f8484e3485f6547e4dc5
unknown <gazinafisrafi.gnr@gmail.com>
2025-02-11 00:15:55 +0600

    lists and list methods practiced

---
+++ lists.py
@@ -0,0 +1,14 @@
+names=["gazi", "nafis", "md", "abdullah"]
+
+print(len(names))
+print(names[-3])
+print(names[3])
+
+# list methods
+names.append("rafi")
+print(names)
+names.reverse()
+names.insert(1, 'gazi')
+print(names)
+names.clear()
+print(names)
