4f8a5dddc1f373b6aba8d522b69dd59f8b8830f9
unknown <gazinafisrafi.gnr@gmail.com>
2025-02-10 18:21:26 +0600

    math operator

---
+++ arithmatic_operator.py
@@ -0,0 +1,21 @@
+x=10
+print(x)
+x= 10 + 10
+print(x)
+y = x + 20
+print(y)
+y = x * y
+print(y)
+y = y - 100
+print(y)
+y = y / 10
+print(y)
+z = y / 3
+y = y // 3
+print(y, z)
+y = y ** 2
+z = z ** 2
+print(y, z)
+
+p = (x+y)*2/z
+print(p)
