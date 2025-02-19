26fa0a7bc0e3e9f01511c22e2fd9ae38c112864d
unknown <gazinafisrafi.gnr@gmail.com>
2025-02-19 18:03:00 +0600

    basic python revision, tuples

--- tuples.py
+++ tuples.py
@@ -1,2 +1,10 @@
 numbers=(1,2,3,4)
+print(numbers.index(3))
 print(numbers.count(1))
+
+grades = ('A+', 'A', 'A-', 'B', 'C', 'D', 'F')
+print(grades)
+print(grades.index('B'))
+print(grades.count('A'))
+for i in grades:
+    print(i)
