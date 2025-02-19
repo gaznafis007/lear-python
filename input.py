737054df2e644384bd06901a20cc4981eb32020a
unknown <gazinafisrafi.gnr@gmail.com>
2025-02-19 16:26:28 +0600

    basic python revision of input

--- input.py
+++ input.py
@@ -2,3 +2,7 @@ name= input("What is your name? ")
 age= input("How old are you? ")
 
 print("Hello", name, "You are " + age + " years old.")
+
+address = input(f'Hey, {name} where do you live?')
+
+print(f'{name} lives in {address}')
