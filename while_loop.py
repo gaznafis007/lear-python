c4da2acab6a59d9bcda9c82f3a5e6136c66f54c8
unknown <gazinafisrafi.gnr@gmail.com>
2025-02-19 17:47:09 +0600

    basic python revision, while loop

--- while_loop.py
+++ while_loop.py
@@ -9,3 +9,23 @@ while i < stars:
 while i >= 0:
     print(i * '*')
     i-=1
+
+def star_sqr_box (size):
+    i = 0
+    while i < int(size):
+        print(size * '*')
+        i+=1
+
+star_sqr_box(stars)
+
+def inverse_stars (size):
+    i = size
+    while (i > 1 ):
+        print(i * '^')
+        i-=1
+
+    while(i <= size):
+        print(i * '^')
+        i+=1
+
+inverse_stars(stars)
