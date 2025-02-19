abc70831dfff7b23a0ce9e0804ab22e6d39feaef
unknown <gazinafisrafi.gnr@gmail.com>
2025-02-19 16:24:15 +0600

    basic python variable revision

--- variables.py
+++ variables.py
@@ -2,4 +2,11 @@ name='John Doe'
 age=26
 is_New_Patient= True
 
-print(name, 'is a patient who is', age, 'years old. and is he a new patient?', is_New_Patient)
+# print(name, 'is a patient who is', age, 'years old. and is he a new patient?', is_New_Patient)
+
+metric_id= 23
+stream = 'science'
+major = 'electrical and electronic'
+is_graduate = True
+
+print(f'{name} is {age} years old, His stream is {stream} and major is {major} his metric id is {metric_id}')
