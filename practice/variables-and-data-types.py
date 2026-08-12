a82570eef96d134bd1679966818e5d7d22d8013b
gaznafis007 <gazinafisrafi.gnr@gmail.com>
2026-08-12 23:56:59 +0600

    streak

--- practice/variables-and-data-types.py
+++ practice/variables-and-data-types.py
@@ -6,3 +6,4 @@ print(f'{name} is {age} years old and is {"a new patient" if not status else "no
 print(f'{name} is {age} years old and is {"adult" if age >= 18 else "not adult"}')
 print(f'Hello, {name}')
 print(f'{name} is {age} years old')
+print(f'{name} is {age} years old and is {"a new patient" if not status else "not a new patient"}')
