14d2320fba34af9f29c8816cb1afb35d295a9552
gaznafis007 <gazinafisrafi.gnr@gmail.com>
2026-08-16 23:24:32 +0600

    streak clearance

--- practice/variables-and-data-types.py
+++ practice/variables-and-data-types.py
@@ -5,5 +5,4 @@ status=True
 print(f'{name} is {age} years old and is {"a new patient" if not status else "not a new patient"}')
 print(f'{name} is {age} years old and is {"adult" if age >= 18 else "not adult"}')
 print(f'Hello, {name}')
-print(f'{name} is {age} years old')
-print(f'{name} is {age} years old and is {"a new patient" if not status else "not a new patient"}')
+
